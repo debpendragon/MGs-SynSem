c4556453cc6f46f88ca647288ca601c614a3964a
Deborah Wong <debpendragon@Deborahs-MacBook-Pro.local>
2018-12-26 17:26:59 -0800

    added merge and check functions

--- MGSyntax.hs
+++ MGSyntax.hs
@@ -33,14 +33,16 @@ head :: Expr -> Expr
 head (Lex (alph, feat)) = (Lex (alph, feat))
 head (Complex (arr, left, right)) = if arr == "<" then head left else head right
 
+--merge function for derived trees
 merge :: Expr -> Expr -> Expr
-merge (Lex (alph, featl)) (Lex (alph, featr)) = 
+merge (Lex (alph, featl)) (Lex (alph, featr)) = undefined 
 
 
 merge (Lex (alph, feat)) (Complex (arr, left, right)) = undefined
 merge (Complex (arr, left, right)) (Lex (alph, feat)) = undefined
 merge (Complex (arr, left, right)) (Complex (arr, left, right)) = undefined
 
+--check whether features match
 check :: [Feat] -> [Feat] -> Bool
 check [] x = False
 check x [] = False
