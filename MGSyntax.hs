27ec715760a91a8321e01529e6646b5a38df8416
yzgncx <tyler.w.carson@gmail.com>
2018-12-26 17:24:18 -0800

    check function

--- MGSyntax.hs
+++ MGSyntax.hs
@@ -34,8 +34,17 @@ head (Lex (alph, feat)) = (Lex (alph, feat))
 head (Complex (arr, left, right)) = if arr == "<" then head left else head right
 
 merge :: Expr -> Expr -> Expr
-merge (Lex (alph, feat)) (Lex (alph, feat)) = undefined
+merge (Lex (alph, featl)) (Lex (alph, featr)) = 
+
+
 merge (Lex (alph, feat)) (Complex (arr, left, right)) = undefined
 merge (Complex (arr, left, right)) (Lex (alph, feat)) = undefined
 merge (Complex (arr, left, right)) (Complex (arr, left, right)) = undefined
 
+check :: [Feat] -> [Feat] -> Bool
+check [] x = False
+check x [] = False
+check (fl:fls) (fr:frs) = if 
+    fl == (Cat f) && fr == (Sel f) ||
+    fl == (Sel f) && fr == (Cat f) then True else False
+    
