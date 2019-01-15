9b12fd330337523962587d478c919466dba5f204
Deborah Wong <debpendragon@Deborahs-MacBook-Pro.local>
2019-01-14 16:40:46 -0800

    empty commit

--- MGSyntax.hs
+++ MGSyntax.hs
@@ -6,9 +6,9 @@ import qualified Data.Set as Set
 	
 type Alph = String
 
-type Sel = String
+type Sel = String 
 
-type Lic = String
+type Lic = String 
 
 data Feat = Pos Lic | Neg Lic | Cat Sel | Eql Sel deriving (Show)
 
@@ -34,19 +34,29 @@ head (Lex (alph, feat)) = (Lex (alph, feat))
 head (Complex (arr, left, right)) = if arr == "<" then head left else head right
 
 --merge function for derived trees
-merge :: Expr -> Expr -> Expr
-merge (Lex (alph, featl)) (Lex (alph, featr)) = undefined 
+{-merge :: Expr -> Expr -> Expr
+merge (Lex (alph, featl)) (Lex (alph, featr)) = undefined
 
 
 merge (Lex (alph, feat)) (Complex (arr, left, right)) = undefined
 merge (Complex (arr, left, right)) (Lex (alph, feat)) = undefined
 merge (Complex (arr, left, right)) (Complex (arr, left, right)) = undefined
+-}
 
 --check whether features match
 check :: [Feat] -> [Feat] -> Bool
 check [] x = False
 check x [] = False
 check (fl:fls) (fr:frs) = if 
-    fl == (Cat f) && fr == (Sel f) ||
-    fl == (Sel f) && fr == (Cat f) then True else False
+    fl == (Cat String) && fr == (Eql String) ||
+    fl == (Eql String) && fr == (Cat String) then True else False
+
+--testing check function
+feat1 = [Eql "d", Cat "V"]
+feat2 = [Cat "d", Neg "k"]
+feat3 = [Cat "v", Cat "V"]
+
+
+
+
     
