01d0d0bb6e958c9ee0e0aaf9b4481e39b5c2e495
Wenyue Hua <wenyuehua@Wenyues-MacBook-Pro.local>
2019-01-15 17:54:31 -0800

    some move function

--- MGSyntax.hs
+++ MGSyntax.hs
@@ -44,6 +44,7 @@ merge (Complex (arr, left, right)) (Lex (alph, feat)) = undefined
 merge (Complex (arr, left, right)) (Complex (arr, left, right)) = undefined
 -}
 
+{--
 --check whether features match--
 check :: [Feat] -> [Feat] -> Bool
 check [] x = False
@@ -51,6 +52,8 @@ check x [] = False
 check (fl:fls) (fr:frs) = if 
     fl == (Cat String) && fr == (Eql String) ||
     fl == (Eql String) && fr == (Cat String) then True else False
+
+    --}
 
 --testing check function
 feat1 = [Eql "d", Cat "V"]
@@ -65,34 +68,35 @@ feat3 = [Cat "v", Cat "V"]
 --merge function for derived trees--
 merge :: Expr -> Expr -> Maybe Expr
 merge (Lex (alphl, featl)) (Lex (alphr, featr)) = 
-	if (check featl featr)
+	if (checkmerge featl featr)
 		then if（headof featl featr == featl）
-			 then (left, (alph1, cutoff featl), (alphr, cutoff featr) )
-			 else (right, (alphr, cutoff featr), (alph1, cutoff featl) )
+			 then Just (left, (alph1, cutoff featl), (alphr, cutoff featr) )
+			 else Just (right, (alphr, cutoff featr), (alph1, cutoff featl) )
 	else Nothing
 
 merge (Lex (alph, feat)) (Complex (arr, left, right)) = 
-	if (check feat extractfeature (Complex (arr, left, right)))
+	if (checkmerge feat extractfeature (Complex (arr, left, right)))
 		then if（headof feat extractfeature (Complex (arr, left, right)) == feat）
-			 then (left, (alph, cutoff feat), newtree (Complex (arr, left, right)) )
-			 else (right, newtree (Complex (arr, left, right)), (alph, cutoff feat )
+			 then Just (left, (alph, cutoff feat), newtree (Complex (arr, left, right)) )
+			 else Just (right, newtree (Complex (arr, left, right)), (alph, cutoff feat )
 	else Nothing
 
 merge (Complex (arr, left, right)) (Lex (alph, feat)) = 
-    if (check extractfeature (Complex (arr, left, right)) feat)
+    if (checkmerge extractfeature (Complex (arr, left, right)) feat)
 		then if （headof feat extractfeature (Complex (arr, left, right)) == feat）
-			 then (left, (alph, cutoff feat), newtree (Complex (arr, left, right)) )
-			 else (right, newtree (Complex (arr, left, right)), (alph, cutoff feat) )
+			 then Just (left, (alph, cutoff feat), newtree (Complex (arr, left, right)) )
+			 else Just (right, newtree (Complex (arr, left, right)), (alph, cutoff feat) )
 	else Nothing
 
 
 merge (Complex (arrl, left1, right1)) (Complex (arrr, left2, right2)) = 
-    if (check extractfeature (Complex (arr, left, right)) extractfeature (Complex (arrr, left2, right2)))
+    if (checkmerge extractfeature (Complex (arr, left, right)) extractfeature (Complex (arrr, left2, right2)))
 	    then if (headof extractfeature (Complex (arr, left, right)) extractfeature (Complex (arrr, left2, right2)) == extractfeature (Complex (arr, left, right)))
-	         then (left, newtree (Complex (arrl, left1, right1)), newtree (Complex (arrr, left2, right2)))
-		     else (right, newtree (Complex (arrr, left2, right2)), newtree (Complex (arrr, left1, right1)))
+	         then Just (left, newtree (Complex (arrl, left1, right1)), newtree (Complex (arrr, left2, right2)))
+		     else Just (right, newtree (Complex (arrr, left2, right2)), newtree (Complex (arrr, left1, right1)))
     else Nothing
 
+--helper functions of MOVE function--
 --get the feature of the head of merging pair--
 headof :: [Feat] -> [Feat] -> Maybe [Feat]
 headof [] x = Nothing
@@ -110,13 +114,22 @@ cutoff [] = []
 cutoff x:xs = xs
 
 --get the features of a tree with the assumption that the feature of a tree is the concatenation of the features of its subtrees--
-extractfeature:: Expr -> [Feat]
+extractfeature :: Expr -> [Feat]
 extractfeature Lex (Alph, [Feat]) = [Feat]
 extractfeature Complex (Arrow, Tree1, Tree2) = (extractfeature Tree1) + (extractfeature Tree2)
 
-newtree:: Expr -> Expr
-newtree (Lex (alph, feat)) = undefined
-newtree (Complex (Arrow, Tree1, Tree2)) =   (Arrow, newtree Tree1, newtree Tree2)
+--when merging trees, we need to delete the feature of a leaf in the tree--
+newtree  :: Expr -> Expr
+newtree (Lex (alph, feat)) = (Lex (alph, cutoff feat))
+newtree (Complex (Arrow, Tree1, Tree2)) =   (Arrow, newtree Tree1, Tree2)
+
+--check whether features match--
+checkmerge :: [Feat] -> [Feat] -> Bool
+checkmerge [] x = False
+checkmerge x [] = False
+checkmerge ((Cat fl):fls) ((Eql fr):frs) = if (fl == fr) then True else False     
+checkmerge ((Eql fl):fls) ((Cat fr):frs) = if (fl == fr) then True else False     
+checkmerge _ _ = False
 
 {--
 --renew the features of a tree--
@@ -137,10 +150,48 @@ newtree (Complex (Arrow, Tree1, Tree2)) =   (Arrow, newtree Tree1, newtree Tree2
  		--(Complex (Arrow, Treel, repfeat(Treer, cutoff extractfeature Treer))
 
 -}
-    --check whether features match--
-check :: [Feat] -> [Feat] -> Bool
-check [] x = False
-check x [] = False
-check ((Cat fl):fls) ((Eql fr):frs) = if (fl == fr) then True else False     
-check ((Eql fl):fls) ((Cat fr):frs) = if (fl == fr) then True else False     
-check _ _ = False
+
+
+=======
+--move function for derived trees--
+{--
+
+--the function takes the whole tree and the licensor, outputs a moved tree--
+move ::Expr -> Expr
+move (Lex (alph, feat)) = (Lex (alph, feat))
+move (Complex (Arrow, Treel, Treer)) = 
+let featseq = extractfeature Treel + extractfeature Treer in
+if ((searchmover featseq Complex (Arrow, Treel, Treer)))) != Nothing)
+	then let mover = searchmover featseq (Complex (Arrow, Treel, Treer)) in
+	(right, newtree mover, lefttree mover (Complex (Arrow, Treel, Treer)))
+else (Arrow, move Treel, move Treer)
+
+--helper functions of move function--
+
+--the function looks for the subtree that matches the feature of the licensor in some range--
+--it takes a feature and a tree, returns the mover--
+searchmover::[Feat] -> Expr -> Maybe Expr
+searchmover [] (Lex (alph, feat)) = Nothing
+searchmover x:xs (Lex (alph, feat)) = Nothing
+searchmover [] (Complex (Arrow, Treel, Treer)) = Nothing
+searchmover x:xs (Complex (Arrow, Treel, Treer)) = 
+	--a binary search here--
+
+
+
+
+
+
+--the function checks where a tree belongs to--
+lefttree::Expr -> Expr -> Expr
+
+
+
+
+
+
+
+
+
+
+--}
